daba9b5b2056cd199f4b7eda43df1e36aca952c1
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-12-02 18:57:08 +0100

    desktop

--- packages/base/install.sh
+++ packages/base/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Installing base packages..."
+
 sudo pacman -S --noconfirm --needed \
   uwsm \
   git \
@@ -9,3 +11,7 @@ sudo pacman -S --noconfirm --needed \
   vim \
   nano \
   yay
+
+echo "Copy default desktop entities..."
+mkdir -p ~/.local/share/applications
+cp -r $ARCHLAND_PATH/defaults/apps/* ~/.local/share/applications/
--- packages/waybar/install.sh
+++ packages/waybar/install.sh
@@ -4,3 +4,8 @@ echo "Installing Waybar and dependencies..."
 sudo pacman -S --noconfirm --needed \
   waybar
 
+echo "Setting up Waybar configuration..."
+rm -rf ~/.config/waybar
+mkdir -p ~/.config/waybar
+cp -r $ARCHLAND_PATH/packages/waybar/config/* ~/.config/waybar
+
