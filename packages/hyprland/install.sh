55d2766c2e596e470b47ee72722a294392bdce02
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-12-02 17:22:52 +0100

    wallpaper

--- packages/hyprland/install.sh
+++ packages/hyprland/install.sh
@@ -2,9 +2,14 @@
 
 echo "Installing Hyprland and dependencies..."
 sudo pacman -S --noconfirm --needed \
-  hyprland
+  hyprland \
+  hyprpaper
 
 echo "Setting up Hyprland configuration..."
 rm -rf ~/.config/hypr
 mkdir -p ~/.config/hypr
 cp -r $ARCHLAND_PATH/packages/hyprland/config/* ~/.config/hypr
+
+echo "Setting up Hyprpaper to start on Hyprland launch..."
+mkdir -p ~/.config/archland
+cp $ARCHLAND_PATH/defaults/wallpaper/1.jpg ~/.config/archland/wallpaper
