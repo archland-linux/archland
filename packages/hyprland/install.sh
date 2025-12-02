664e9f28f707495818760ece1a22c01215aabc53
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-12-02 15:45:51 +0100

    fix hyprland

--- packages/hyprland/install.sh
+++ packages/hyprland/install.sh
@@ -7,4 +7,5 @@ sudo pacman -S --noconfirm --needed \
 echo "Archland path: $ARCHLAND_PATH"
 echo "Setting up Hyprland configuration..."
 rm -rf ~/.config/hypr
+mkdir -p ~/.config/hypr
 cp -r $ARCHLAND_PATH/packages/hyprland/config ~/.config/hypr
